3a9d197ec2a9f584968f2df72cf516188d8f7895
mursit boutique <mursit@boutiquerugs.com>
2022-11-24 18:46:10 +0000

    Exercise 12.10

--- todo-app/todo-backend/routes/statistics.js
+++ todo-app/todo-backend/routes/statistics.js
@@ -1,19 +1,12 @@
 const express = require('express')
-require('dotenv').config()
-
 const router = express.Router()
+const redis = require('../redis')
 
-const configs = require('../util/config')
-
-let visits = 0
-
-/* GET index data. */
 router.get('/', async (req, res) => {
-  visits++
+  const count = (await redis.getAsync('added_todos')) || 0
 
   res.send({
-    ...configs,
-    visits,
+    added_todos: count,
   })
 })
 
